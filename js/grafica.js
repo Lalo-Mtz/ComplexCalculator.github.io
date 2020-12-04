341abbd5ba10cc4252b26617c54edf4a6a6e1ec0
Lalo-Mtz <gerardocct@gmail.com>
2020-12-04 14:07:17 -0600

    Correccion cambio de escala en la grafica

--- js/grafica.js
+++ js/grafica.js
@@ -1,7 +1,7 @@
 let x=0, y=0;
 let l=5, k=5;
 let j=10, h=10; 
-
+let x1=0, y1=0;
 
 function setup(){
     let cv = createCanvas(450,410);
@@ -25,41 +25,49 @@ function draw(){
         if(o>10000){
             l = 50000;
             j = 100000;
-            x = x/10000;
+            x1 = x/10000;
         }else if(o>1000){
             l = 5000;
             j = 10000;
-            x = x/1000;
+            x1 = x/1000;
         }else if(o>100){
             l = 500;
             j = 1000;
-            x = x/100;
+            x1 = x/100;
         }else if(o>10){
             l = 50;
             j = 100;
-            x = x/10;
+            x1 = x/10;
+        }else if(o>0){
+            l = 5;
+            j = 10;
+            x1 = x;
         }
     
         if(p>10000){
             k = 50000;
             h = 100000;
-            y = y/10000;
+            y1 = y/10000;
         }else if(p>1000){
             k = 5000;
             h = 10000;
-            y = y/1000;
+            y1 = y/1000;
         }else if(p>100){
             k = 500;
             h = 1000;
-            y = y/100;
+            y1 = y/100;
         }else if(p>10){
             k = 50;
             h = 100;
-            y = y/10;
+            y1 = y/10;
+        }else if(p>0){
+            k = 5;
+            h = 10;
+            y1 = y;
         }
     }
     
-
+    clear();
     text(`${l}`, ((10+5)*20)+22, 215, 80, 80);
     text(`- ${l}`, ((10-5)*20)+15, 215, 80, 80);
 
@@ -90,25 +98,25 @@ function draw(){
     
 
     //if(x!=0 && y!=0){
-        line(225, 200, ((10+x)*20)+25, ((10-y)*20));
+        line(225, 200, ((10+x1)*20)+25, ((10-y1)*20));
 
         stroke('blue');
-        line(((10+x)*20)+25, 200, ((10+x)*20)+25, ((10-y)*20));
+        line(((10+x1)*20)+25, 200, ((10+x1)*20)+25, ((10-y1)*20));
 
-        line((10*20)+25, ((10-y)*20), ((10+x)*20)+25, ((10-y)*20));
+        line((10*20)+25, ((10-y1)*20), ((10+x1)*20)+25, ((10-y1)*20));
 
         stroke('green');
-        if(x<0 && y<0)
-            arc(225, 200, 120, 120, -Math.atan(y/x)+Math.PI, Math.PI*2);
-        else if(x<0 && y>0)
-            arc(225, 200, 120, 120, -Math.atan(y/x)-Math.PI, Math.PI*2);
+        if(x1<0 && y1<0)
+            arc(225, 200, 120, 120, -Math.atan(y1/x1)+Math.PI, Math.PI*2);
+        else if(x1<0 && y1>0)
+            arc(225, 200, 120, 120, -Math.atan(y1/x1)-Math.PI, Math.PI*2);
         else
-            arc(225, 200, 120, 120, -Math.atan(y/x), Math.PI*2);
+            arc(225, 200, 120, 120, -Math.atan(y1/x1), Math.PI*2);
         
 
         stroke('red');
         strokeWeight(10);
-        point(((10+x)*20)+25, ((10-y)*20));
+        point(((10+x1)*20)+25, ((10-y1)*20));
     //}
 
 }
